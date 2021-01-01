7a274839fbb5f36c52097c46d3fe4d88ecb35402
lakindu <64328828+lakindu-banneheka@users.noreply.github.com>
2021-01-01 21:30:29 +0530

    basic things

--- src/App.js
+++ src/App.js
@@ -3,11 +3,28 @@ import React from 'react';
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/logo/logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+import Rank from './components/Rank/Rank';
+import Particles from 'react-particles-js';
+
+const ParticleOptions = {
+  particles: {
+    line_linked: {
+      shadow: {
+        enable: true,
+        color: "#3CA9D1",
+        blur: 5
+      }
+    }
+  }
+}
+
 function App() {
   return (
     <div className="App">
+      <Particles params={ParticleOptions} className="particles" />
       <Navigation />
       <Logo />
+      <Rank />
       <ImageLinkForm />
     </div>
   );
--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import '../../App.css'
+import './ImageLinkForm.css'
 
 const ImageLinkForm = () =>{
     return(
@@ -7,12 +8,12 @@ const ImageLinkForm = () =>{
             <p className='f3'>
                 {'This Magic brain will detect faces in your pictures. Git it a try'}
             </p>
-            <div className='Center'>
-                <div className='pa4 br3 shadow-5' >
-                <input type='tex' className='f4 pa2 w-70 center' />
+            <form className='center'>
+                <div className='form  Center pa4 br3 shadow-5' >
+                <input type='text' className='f4 pa2 w-70 center text-box ' />
                 <button className='w-30 grow f4 link ph3 pv2 dib white bg-light-purple'>Detect</button>
                 </div>
-            </div>
+            </form>
         </div>
     );
 }
